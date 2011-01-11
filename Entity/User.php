55c65d13eac60b39212f55c9b4fce8acaa7af34f
Thomas <thomas.rabaix@soleoweb.com>
2011-01-11 01:25:16 +0100

    start converting into an easy extends bundle

--- Entity/User.php
+++ Entity/User.php
@@ -15,4 +15,103 @@ use Bundle\FOS\UserBundle\Model\User as AbstractUser;
 abstract class User extends AbstractUser
 {
 
+    /**
+     * Set usernameLower
+     *
+     * @param string $usernameLower
+     */
+    public function setUsernameLower($usernameLower)
+    {
+        $this->usernameLower = $usernameLower;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean $enabled
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Get locked
+     *
+     * @return boolean $locked
+     */
+    public function getLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
+     * Get expired
+     *
+     * @return boolean $expired
+     */
+    public function getExpired()
+    {
+        return $this->expired;
+    }
+
+    /**
+     * Get expiresAt
+     *
+     * @return datetime $expiresAt
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * Get credentialsExpired
+     *
+     * @return boolean $credentialsExpired
+     */
+    public function getCredentialsExpired()
+    {
+        return $this->credentialsExpired;
+    }
+
+    /**
+     * Get credentialsExpireAt
+     *
+     * @return datetime $credentialsExpireAt
+     */
+    public function getCredentialsExpireAt()
+    {
+        return $this->credentialsExpireAt;
+    }
 }
